8167d74ebaf04a19c3b38688f31ff542ece06ff3
808289 <rajadhurai322@gmail.com>
2023-01-06 01:28:19 +0530

    Initial Commit

---
+++ pr1.js
@@ -0,0 +1,31 @@
+const a = document.querySelector('#Searchform');
+const aa = document.querySelector('#Aa');
+const aaa = document.querySelector('#nn');
+a.addEventListener('submit', async function(e) {
+    e.preventDefault();
+    const b = a.elements.query.value;
+    if (a.elements.query.value) {
+        aa.innerHTML = ""
+    }
+    const c = { params: { q: b } }
+    const r = await axios.get(`http://api.tvmaze.com/search/shows`, c)
+    mi(r.data)
+        /* const img = document.createElement('IMG')
+         img.src = r.data[0].show.image.medium
+         document.body.append(img)*/
+    if (a.elements.query.value == "") {
+        aa.innerHTML = ""
+    }
+})
+aaa.addEventListener('click', function() {
+    a.elements.query.value = ""
+})
+const mi = (ss) => {
+    for (let a1 of ss) {
+        if (a1.show.image) {
+            const i = document.createElement('IMG')
+            i.src = a1.show.image.medium
+            aa.append(i)
+        }
+    }
+}
