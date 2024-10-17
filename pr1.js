f863b6033946a6b3872192b0241095308b591116
808289 <rajadhurai322@gmail.com>
2024-10-17 12:07:13 +0530

    latest commit

--- pr1.js
+++ pr1.js
@@ -1,33 +1,28 @@
-const a = document.querySelector("#Searchform");
-const aa = document.querySelector("#Aa");
-const aaa = document.querySelector("#nn");
-a.addEventListener("submit", async function (e) {
+const input = document.querySelector("#Searchform");
+const addTheList = document.querySelector("#addTheList");
+const clear = document.querySelector("#clear");
+input.addEventListener("submit", async function (e) {
   e.preventDefault();
-  console.log("hello");
-  const b = a.elements.query.value;
-  if (a.elements.query.value) {
-    aa.innerHTML = "";
+  const b = input.elements.query.value;
+  if (input.elements.query.value) {
+    addTheList.innerHTML = "";
   }
   const c = { params: { q: b } };
   const r = await axios.get(`http://api.tvmaze.com/search/shows`, c);
   mi(r.data);
-  /* const img = document.createElement('IMG')
-         img.src = r.data[0].show.image.medium
-         document.body.append(img)*/
-  if (a.elements.query.value == "") {
-    aa.innerHTML = "";
+  if (input.elements.query.value == "") {
+    addTheList.innerHTML = "";
   }
 });
-aaa.addEventListener("click", function () {
-  a.elements.query.value = "";
+clear.addEventListener("click", function () {
+  input.elements.query.value = "";
 });
 const mi = (ss) => {
   for (let a1 of ss) {
     if (a1.show.image) {
       const i = document.createElement("IMG");
       i.src = a1.show.image.medium;
-      aa.append(i);
+      addTheList.append(i);
     }
   }
-  console.log(aa);
 };
